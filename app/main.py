7e3909e120902a204a2b901161edf8406b06386f
Ruslan Ivliev <148155200+Miikey666@users.noreply.github.com>
2024-10-25 23:19:13 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,4 @@
-from dbm import error
-from typing import Set, Dict, Any
-
-def format_linter_error(error: dict) -> dict[str, str | Any]:
+def format_linter_error(error: dict) -> dict:
     return {
         "line": error["line_number"],
         "column": error["column_number"],
@@ -9,6 +6,7 @@ def format_linter_error(error: dict) -> dict[str, str | Any]:
         "name": error["code"],
         "source": "flake8"
     }
+
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
@@ -25,6 +23,7 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
         "path": file_path,
         "status": "failed" if errors else "passed"
     }
+
 
 def format_linter_report(linter_report: dict) -> list:
     return [
